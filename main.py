c9f03d192a61b9893503f2c1ad3405f109be6f4b
Alexandre <alexandrelimaxxxx@gmail.com>
2024-05-28 23:38:17 -0300

    update new endpoints

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify
-from google.cloud import storage
+from google.cloud import storage, bigquery
+from dotenv import load_dotenv
 import os
+load_dotenv()
+PROJECT_ID = os.environ.get("PROJECT_ID")
+DATASET_ID = os.environ.get("DATASET_ID")
+TABLE = os.environ.get("TABLE")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'service_account.json'
 
 app = Flask(__name__)
 
@@ -36,6 +42,41 @@ def download_file():
 
     return jsonify('File downloaded successfully')
 
+@app.route('/query', methods=['GET'])
+def query_bigquery():
+    client = bigquery.Client()
+    query = f"""
+        SELECT * FROM `{PROJECT_ID}.{DATASET_ID}.{TABLE}`
+        LIMIT 1000
+    """
+    data = client.query(query)
+    results = data.result()
+
+    results_list = [
+        {"id": row["id"], "nome": row["nome"], "idade": row["idade"]}
+        for row in results
+    ]
+
+    return jsonify(results_list)
+
+@app.route('/insert', methods=['POST'])
+def insert_data_into_bigquery():
+    try:
+        client = bigquery.Client()
+        user_data = request.json
+
+        table_ref = client.dataset(DATASET_ID).table(TABLE)
+        table = client.get_table(table_ref)
+        client.insert_rows(table, [user_data])
+
+        return jsonify({"success": True, "message": "Os registros foram inseridos na tabela com sucesso."})
+
+
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 500
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
+
